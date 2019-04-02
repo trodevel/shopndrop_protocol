a1ee1229467d202c1eaab656a89a57678bdae6b0
trodevel <trodevel@gmail.com>
2019-04-02 02:20:11 +0200

    intermediate results

--- shopndrop_protocol.h
+++ shopndrop_protocol.h
@@ -19,7 +19,7 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 */
 
-// $Revision: 10697 $ $Date:: 2019-03-31 #$ $Author: serge $
+// $Revision: 10709 $ $Date:: 2019-04-01 #$ $Author: serge $
 
 #ifndef LIB_SHOPNDROP_PROTOCOL_SHOPNDROP_PROTOCOL_H
 #define LIB_SHOPNDROP_PROTOCOL_SHOPNDROP_PROTOCOL_H
@@ -29,9 +29,7 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 #include <vector>               // std::vector
 
 #include "generic_protocol/generic_protocol.h"   // generic_protocol::Request
-#include "basic_objects/basic_objects.h" // basic_objects::TimeWindow
-#include "voice_enum.h"         // voice_e
-#include "urgency_enum.h"       // urgency_e
+#include "basic_objects/basic_objects.h" // basic_objects::LocalTime
 
 namespace shopndrop_protocol {
 
@@ -41,6 +39,10 @@ struct Request: public generic_protocol::Request
 };
 
 typedef uint32_t    id_t;
+
+/**************************************************
+ * OBJECTS
+ **************************************************/
 
 struct ProductItem
 {
@@ -56,27 +58,11 @@ struct ShoppingItem
     uint32_t        amount;
 };
 
-namespace web
-{
-struct ShoppingItem
-{
-    ::shopndrop_protocol::ShoppingItem shopping_item;
-    ProductItem     product_item;
-};
-}
-
 struct ShoppingList
 {
     std::vector<ShoppingItem>   items;
 };
 
-namespace web
-{
-struct ShoppingList
-{
-    std::vector<ShoppingItem>   items;
-};
-}
 
 struct Ride
 {
@@ -91,31 +77,13 @@ struct RideWithId
     Ride            ride;
 };
 
-namespace web
+enum class order_status_e
 {
-struct RideWithRequests
-{
-    RideWithId      ride;
-    uint32_t        num_requests;
-};
-}
-
-namespace web
-{
-struct RideRequest
-{
-    id_t            request_id;
-    double          sum;
-    double          earning;
-    double          weight;
-    uint32_t        plz;
-    std::string     address;
-};
-}
-
-struct GetPersonalUserInfoRequest: public Request
-{
-    id_t            user_id;
+    CANCELLED                   = 0,
+    WAITING_ACCEPTANCE          = 1,
+    ACCEPTED_WAITING_DELIVERY   = 2,
+    DELIVERED_WAITING_FEEDBACK  = 3,
+    CLOSED_FEEDBACK_RECEIVED    = 4,
 };
 
 enum class gender_e
@@ -125,19 +93,117 @@ enum class gender_e
     FEMALE  = 2,
 };
 
+struct Address
+{
+    uint32_t    plz;
+    std::string city;
+    std::string street;
+    std::string house_number;
+    std::string extra_address_line;
+    std::string country;
+};
+
+/**************************************************
+ * WEB OBJECTS
+ **************************************************/
+
+namespace web
+{
+
+struct ShoppingItem
+{
+    ::shopndrop_protocol::ShoppingItem shopping_item;
+    ProductItem     product_item;
+};
+
+struct ShoppingList
+{
+    std::vector<ShoppingItem>   items;
+};
+
+struct ShoppingListWithTotals
+{
+    ShoppingList    shopping_list;
+    double          price;
+    double          weight;
+};
+
+struct RideWithRequests
+{
+    RideWithId      ride;
+    uint32_t        num_requests;
+};
+
+struct RideOrderInfo
+{
+    id_t            order_id;
+    double          sum;
+    double          earning;
+    double          weight;
+    uint32_t        plz;
+    std::string     address;
+};
+
+struct AcceptedOrder
+{
+    id_t            order_id;
+    basic_objects::LocalTime delivery_time;
+    std::string     address;
+    uint32_t        plz;
+    id_t            shopping_list_id;
+    double          sum;
+    double          earning;
+    double          weight;
+    order_status_e  status;
+};
+
+struct DashScreenShopper
+{
+    basic_objects::LocalTime        current_time;
+
+    std::vector<RideWithRequests>   rides;
+    std::vector<AcceptedOrder>      orders;
+};
+
+}   // namespace web
+
+/**************************************************
+ * REQUESTS
+ **************************************************/
+
+struct GetPersonalUserInfoRequest: public Request
+{
+    id_t            user_id;
+};
+
 struct GetPersonalUserInfoResponse: public generic_protocol::BackwardMessage
 {
     id_t            user_id;
     gender_e        gender;
-    std::string     name;
     std::string     first_name;
+    std::string     last_name;
     std::string     company_name;
     std::string     email;
-    std::string     email_2;
     std::string     phone;
-    std::string     phone_2;
     std::string     timezone;
 };
+
+/**************************************************
+ * WEB REQUESTS
+ **************************************************/
+
+namespace web
+{
+struct GetRideOrderInfoRequest: public Request
+{
+    id_t            ride_id;
+};
+
+struct GetRideOrderInfoResponse: public generic_protocol::BackwardMessage
+{
+    std::vector<RideOrderInfo>    rides;
+};
+}
 
 } // namespace shopndrop_protocol
 
