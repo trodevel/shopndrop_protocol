c711698a85cd564bffbc7838e292ad19b3992c84
trodevel <trodevel@gmail.com>
2019-04-06 00:38:16 +0200

    added RequestParser

---
+++ request_parser.cpp
@@ -0,0 +1,302 @@
+/*
+
+Request Parser.
+
+Copyright (C) 2019 Sergey Kolevatov
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+// $Revision: 10785 $ $Date:: 2019-04-06 #$ $Author: serge $
+
+#include "request_parser.h"         // self
+
+#include "basic_parser/get_value.h" // basic_parser::get_value_or_throw
+#include "basic_objects/parser.h"   // basic_objects::Parser
+
+#include "parser.h"                 // Parser
+#include "generic_protocol/request_parser.h" // generic_protocol::RequestParser::to_request()
+#include "request_validator.h"      // RequestValidator
+
+namespace shopndrop_protocol {
+
+using basic_parser::get_value_or_throw;
+using basic_parser::get_value_or_throw_uint32;
+
+generic_protocol::ForwardMessage* RequestParser::to_forward_message( const generic_request::Request & r )
+{
+    auto type = RequestParser::detect_request_type( r );
+
+    typedef request_type_e KeyType;
+    typedef RequestParser Type;
+
+    typedef ForwardMessage* (*PPMF)( const generic_request::Request & r );
+
+    static const std::map<request_type_e, PPMF> funcs =
+    {
+        { KeyType::AddRideRequest,              & Type::to_AddRideRequest },
+        { KeyType::AddOrderRequest,             & Type::to_AddOrderRequest },
+        { KeyType::CancelOrderRequest,          & Type::to_CancelOrderRequest },
+        { KeyType::GetPersonalUserInfoRequest,  & Type::to_GetPersonalUserInfoRequest },
+        { KeyType::GetRideRequest,              & Type::to_GetRideRequest },
+        { KeyType::GetPersonalUserInfoRequest,  & Type::to_GetPersonalUserInfoRequest },
+    };
+
+    auto it = funcs.find( type );
+
+    if( it != funcs.end() )
+        return it->second( r );
+
+    return generic_protocol::RequestParser::to_forward_message( r );
+}
+
+request_type_e  RequestParser::detect_request_type( const generic_request::Request & r )
+{
+    std::string cmd;
+
+    if( r.get_value( "CMD", cmd ) == false )
+        throw MalformedRequest( "CMD is not defined" );
+
+    return Parser::to_request_type( cmd );
+}
+
+void RequestParser::to_Id( id_t * res, const std::string & key, const generic_request::Request & r )
+{
+    get_value_or_throw_uint32( * res, key, r );
+}
+
+void RequestParser::to_order_status( order_status_e * res, const std::string & key, const generic_request::Request & r )
+{
+    uint32_t status;
+
+    get_value_or_throw_uint32( status, key, r );
+
+    * res = status;
+}
+
+void RequestParser::to_ProductItem( ProductItem * res, const generic_request::Request & r )
+{
+    get_value_or_throw( res->name,          "NAME", r );
+    get_value_or_throw( res->unit,          "UNIT", r );
+    get_value_or_throw_double( res->price,  "PRICE", r );
+    get_value_or_throw_double( res->weight, "WEIGHT", r );
+}
+
+RequestParser::ForwardMessage * RequestParser::to_AddRideRequest( const generic_request::Request & r )
+{
+    auto * res = new AddRideRequest;
+
+    generic_protocol::RequestParser::to_request( res, r );
+
+    to_Ride( & res->ride, r );
+
+    RequestValidator::validate( * res );
+
+    return res;
+}
+
+RequestParser::ForwardMessage * RequestParser::to_CancelRideRequest( const generic_request::Request & r )
+{
+    auto res = new CancelRideRequest;
+
+    generic_protocol::RequestParser::to_request( res, r );
+
+    to_Id( & res->ride_id, "RIDE_ID", r );
+
+    RequestValidator::validate( * res );
+
+    return res;
+}
+
+RequestParser::ForwardMessage * RequestParser::to_GetRideRequest( const generic_request::Request & r )
+{
+    auto * res = new GetRideRequest;
+
+    generic_protocol::RequestParser::to_request( res, r );
+
+    to_Id( & res->ride_id, "RIDE_ID", r );
+
+    RequestValidator::validate( * res );
+
+    return res;
+}
+
+RequestParser::ForwardMessage * RequestParser::to_AddOrderRequest( const generic_request::Request & r )
+{
+    auto * res = new AddOrderRequest;
+
+    generic_protocol::RequestParser::to_request( res, r );
+
+    to_Id( & res->ride_id, "RIDE_ID", r );
+
+    to_ShoppingList( & res->shopping_list, r );
+
+    RequestValidator::validate( * res );
+
+    return res;
+}
+
+RequestParser::ForwardMessage * RequestParser::to_CancelOrderRequest( const generic_request::Request & r )
+{
+    auto * res = new CancelOrderRequest;
+
+    generic_protocol::RequestParser::to_request( res, r );
+
+    to_Id( & res->order_id, "ORDER_ID", r );
+
+    RequestValidator::validate( * res );
+
+    return res;
+}
+
+RequestParser::ForwardMessage * RequestParser::to_GetPersonalUserInfoRequest( const generic_request::Request & r )
+{
+    auto * res = new GetPersonalUserInfoRequest;
+
+    generic_protocol::RequestParser::to_request( res, r );
+
+    to_Id( & res->user_id, "USER_ID", r );
+
+    RequestValidator::validate( * res );
+
+    return res;
+}
+
+
+void RequestParser::to_ShoppingList( ShoppingList * res, const generic_request::Request & r )
+{
+}
+
+RequestParser::ForwardMessage * RequestParser::to_GetPersonalUserInfoRequest( const generic_request::Request & r )
+{
+    auto * res = new GetPersonalUserInfoRequest;
+
+    get_value_or_throw_uint32( res->user_id,       "USER_ID", r );
+
+    generic_protocol::RequestParser::to_request( res, r );
+
+    RequestValidator::validate( * res );
+
+    return res;
+}
+
+void RequestParser::to_Ride( Ride * res, const generic_request::Request & r )
+{
+    get_value_or_throw_uint32( res->plz,       "PLZ", r );
+
+    basic_objects::Parser::to_LocalTime( & res->delivery_time, "DELIVERY_TIME", r );
+
+    get_value_or_throw_double( res->max_weight, "MAX_WEIGHT", r );
+}
+
+void RequestParser::to_Address( Address * res, const generic_request::Request & r )
+{
+    get_value_or_throw_uint32( res->plz,    "PLZ", r );
+
+    get_value_or_throw( res->city,          "CITY", r );
+    get_value_or_throw( res->street,        "STREET", r );
+    get_value_or_throw( res->house_number,  "HOUSE_NUMBER", r );
+    r.get_value( "EAL", res->extra_address_line );
+    get_value_or_throw( res->country,       "COUNTRY", r );
+}
+
+void RequestParser::to_ShoppingItem( ShoppingItem * res, const generic_request::Request & r )
+{
+    to_Id( & res->product_item_id, "PRODUCT_ITEM_ID", r );
+
+    get_value_or_throw_uint32( res->amount,             "AMOUNT", r );
+}
+
+namespace web {
+
+generic_protocol::ForwardMessage* RequestParser::to_forward_message( const generic_request::Request & r )
+{
+    auto type = RequestParser::detect_request_type( r );
+
+    typedef request_type_e KeyType;
+    typedef RequestParser Type;
+
+    typedef ForwardMessage* (*PPMF)( const generic_request::Request & r );
+
+    static const std::map<request_type_e, PPMF> funcs =
+    {
+        { KeyType::GetProductItemListRequest,   & Type::to_GetProductItemListRequest },
+        { KeyType::GetRideOrderInfoRequest,     & Type::to_GetRideOrderInfoRequest },
+        { KeyType::GetDashScreenUserRequest,    & Type::to_GetDashScreenUserRequest },
+        { KeyType::GetDashScreenShopperRequest, & Type::to_GetDashScreenShopperRequest },
+    };
+
+    auto it = funcs.find( type );
+
+    if( it != funcs.end() )
+        return it->second( r );
+
+    return ::shopndrop_protocol::RequestParser::to_forward_message( r );
+}
+
+RequestParser::ForwardMessage * RequestParser::to_GetProductItemListRequest( const generic_request::Request & r )
+{
+    auto * res = new GetProductItemListRequest;
+
+    generic_protocol::RequestParser::to_request( res, r );
+
+    RequestValidator::validate( * res );
+
+    return res;
+}
+
+RequestParser::ForwardMessage * RequestParser::to_GetRideOrderInfoRequest( const generic_request::Request & r )
+{
+    auto * res = new GetRideOrderInfoRequest;
+
+    generic_protocol::RequestParser::to_request( res, r );
+
+    ::shopndrop_protocol::RequestParser::to_Id( & res->ride_id, "RIDE_ID", r );
+
+    RequestValidator::validate( * res );
+
+    return res;
+}
+
+RequestParser::ForwardMessage * RequestParser::to_GetDashScreenUserRequest( const generic_request::Request & r )
+{
+    auto * res = new GetDashScreenUserRequest;
+
+    generic_protocol::RequestParser::to_request( res, r );
+
+    ::shopndrop_protocol::RequestParser::to_Id( & res->user_id, "USER_ID", r );
+
+    RequestValidator::validate( * res );
+
+    return res;
+}
+
+RequestParser::ForwardMessage * RequestParser::to_GetDashScreenShopperRequest( const generic_request::Request & r )
+{
+    auto * res = new GetDashScreenShopperRequest;
+
+    generic_protocol::RequestParser::to_request( res, r );
+
+    ::shopndrop_protocol::RequestParser::to_Id( & res->user_id, "USER_ID", r );
+
+    RequestValidator::validate( * res );
+
+    return res;
+}
+
+
+} // namespace web
+
+} // namespace shopndrop_protocol
---
+++ request_parser.h
@@ -0,0 +1,86 @@
+/*
+
+Request Parser.
+
+Copyright (C) 2019 Sergey Kolevatov
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+// $Revision: 10784 $ $Date:: 2019-04-06 #$ $Author: serge $
+
+#include "generic_request/request.h"        // generic_request::Request
+#include "basic_parser/malformed_request.h" // MalformedRequest
+#include "enums.h"                          // request_type_e
+#include "shopndrop_protocol.h"             // Request...
+
+namespace shopndrop_protocol {
+
+class RequestParser
+{
+public:
+    typedef basic_parser::MalformedRequest      MalformedRequest;
+    typedef generic_protocol::ForwardMessage    ForwardMessage;
+
+public:
+
+    static generic_protocol::ForwardMessage*    to_forward_message( const generic_request::Request & r );
+
+    static void                         to_Id( id_t * res, const std::string & key, const generic_request::Request & r );
+    static void                         to_order_status( order_status_e * res, const std::string & key, const generic_request::Request & r );
+    static void                         to_ProductItem( ProductItem * res, const generic_request::Request & r );
+    static void                         to_ShoppingItem( ShoppingItem * res, const generic_request::Request & r );
+    static void                         to_ShoppingList( ShoppingList * res, const generic_request::Request & r );
+    static void                         to_Ride( Ride * res, const generic_request::Request & r );
+    static void                         to_Address( Address * res, const generic_request::Request & r );
+    //static void                         to_Order( Order * res, const generic_request::Request & r );
+
+private:
+
+    static request_type_e   detect_request_type( const generic_request::Request & r );
+
+    static ForwardMessage *             to_AddRideRequest( const generic_request::Request & r );
+    static ForwardMessage *             to_CancelRideRequest( const generic_request::Request & r );
+    static ForwardMessage *             to_GetRideRequest( const generic_request::Request & r );
+    static ForwardMessage *             to_AddOrderRequest( const generic_request::Request & r );
+    static ForwardMessage *             to_CancelOrderRequest( const generic_request::Request & r );
+    static ForwardMessage *             to_GetPersonalUserInfoRequest( const generic_request::Request & r );
+};
+
+namespace web
+{
+class RequestParser
+{
+public:
+    typedef basic_parser::MalformedRequest      MalformedRequest;
+    typedef generic_protocol::ForwardMessage    ForwardMessage;
+
+public:
+
+    static generic_protocol::ForwardMessage*    to_forward_message( const generic_request::Request & r );
+
+private:
+
+    static request_type_e   detect_request_type( const generic_request::Request & r );
+
+    static ForwardMessage *             to_GetProductItemListRequest( const generic_request::Request & r );
+    static ForwardMessage *             to_GetRideOrderInfoRequest( const generic_request::Request & r );
+    static ForwardMessage *             to_GetDashScreenUserRequest( const generic_request::Request & r );
+    static ForwardMessage *             to_GetDashScreenShopperRequest( const generic_request::Request & r );
+};
+}
+
+} // namespace shopndrop_protocol
+
