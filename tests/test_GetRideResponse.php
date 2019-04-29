e2601db9699920f1dcbc935ca528460d06f7a345
trodevel <trodevel@gmail.com>
2019-04-29 02:00:59 +0200

    added tests

---
+++ tests/test_GetRideResponse.php
@@ -0,0 +1,18 @@
+<?php
+
+// $Revision: 10972 $ $Date:: 2019-04-29 #$ $Author: serge $
+
+require_once '../shopndrop_protocol_web.php';
+require_once '../response_parser_web.php';
+require_once '../html_helper_web.php';
+
+{
+    $resp  =
+    'GetRideResponse;50668;0;0;20190522180000;2.5;';
+
+    $parsed = \shopndrop_protocol\web\ResponseParser::parse( $resp );
+
+    echo "parsed: " . \shopndrop_protocol\web\to_html( $parsed ) . "\n";
+}
+
+?>
