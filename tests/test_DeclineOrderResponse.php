baa0f2443f75337110786bcaaaa531e1853799da
trodevel <trodevel@gmail.com>
2019-05-12 02:22:57 +0200

    added examples

---
+++ tests/test_DeclineOrderResponse.php
@@ -0,0 +1,18 @@
+<?php
+
+// $Revision: 11274 $ $Date:: 2019-05-12 #$ $Author: serge $
+
+require_once '../shopndrop_protocol_web.php';
+require_once '../response_parser_web.php';
+require_once '../html_helper_web.php';
+
+{
+    $resp  =
+    'DeclineOrderResponse;';
+
+    $parsed = \shopndrop_protocol\web\ResponseParser::parse( $resp );
+
+    echo "parsed: " . \shopndrop_protocol\web\to_html( $parsed ) . "\n";
+}
+
+?>
