07b99d97c0045aab0dd22370931aabac29e69bb2
trodevel <trodevel@gmail.com>
2019-05-18 03:53:59 +0200

    added init_...() for ShoppingRequestInfo

--- response_gen.h
+++ response_gen.h
@@ -19,7 +19,7 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 */
 
-// $Revision: 11309 $ $Date:: 2019-05-13 #$ $Author: serge $
+// $Revision: 11494 $ $Date:: 2019-05-17 #$ $Author: serge $
 
 #ifndef LIB_SHOPNDROP_PROTOCOL_RESPONSE_GEN_H
 #define LIB_SHOPNDROP_PROTOCOL_RESPONSE_GEN_H
@@ -244,6 +244,24 @@ inline DashScreenShopper * init_DashScreenShopper(
     res->current_time   = current_time;
     res->rides          = rides;
     res->orders         = orders;
+
+    return res;
+}
+
+
+inline ShoppingRequestInfo * init_ShoppingRequestInfo(
+        ShoppingRequestInfo * res,
+        id_t                order_id,
+        double              sum,
+        double              earning,
+        double              weight,
+        Address             address )
+{
+    res->order_id   = order_id;
+    res->sum        = sum;
+    res->earning    = earning;
+    res->weight     = weight;
+    res->address    = address;
 
     return res;
 }
